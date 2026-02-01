7a655c1f9abe58842e31147aff50bbe29cb53ca2
Deepali <joysingal247@gmail.com>
2026-02-01 16:16:21 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import StateContextProvider from "./context/stateContextProvider.jsx";
+import StateContextProvider from "./context/StateContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
